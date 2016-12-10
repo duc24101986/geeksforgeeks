4ca8740b062f3c5419e5b97a8960ee03094aecf2
duc24101986 <duc24101986@gmail.com>
2016-12-10 10:08:08 +0000

    BFS and graph

---
+++ graph/bfs.py
@@ -0,0 +1,41 @@
+import Queue
+from graph import Vertex, Graph
+
+def createGraph():
+    g = Graph()
+    
+    for index in range(10):
+        g.addVertex(index)
+    
+    for start in range(1, 9):
+            g.addEdge(start,start+1,1)
+            g.addEdge(start+1,start,1)
+    
+    return g
+    
+def bfs(graph, start):
+    vetices_queue = Queue.Queue()
+    vetices_queue.put(start)
+    
+    path= []
+
+    while(not vetices_queue.empty()):
+        # depop the queue
+        current_vertex = vetices_queue.get()
+        # mark the vertex as visited
+        path.append(current_vertex)
+        # Find the nbr
+        nbr = graph.getVertex(current_vertex).connectedTo.keys()
+        
+        for nbr_vertex in nbr:
+            if nbr_vertex not in path:
+                
+                vetices_queue.put(nbr_vertex)
+                
+    print path
+    
+if __name__ == '__main__':
+
+    g = createGraph()
+    g.printGraph()
+    bfs(g, 5)
--- graph/graph.py
+++ graph/graph.py
@@ -1,6 +1,71 @@
-array = [5, 4, 12, 7, 15, 9]
-array_length = len(array)
+import Queue
+# The graph class :
+# vertex_list = {1: Vertex_object, 2: Vertex_object, 3: Vertex_object}
+# vertex_object contains id and connectedTo = {1: weight, 2: weigth, 3: weight}
 
-for i in range(array_length-1, -1, -1):
-	
-	print array[i]
+class Vertex:
+    def __init__(self,key):
+        self.id = key
+        self.connectedTo = {}
+    
+    def addNeighbor(self, nbr, weight=0):
+        self.connectedTo[nbr] = weight
+    
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self,nbr):
+        return self.connectedTo[nbr]
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+    
+    def addVertex(self, key):
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        self.numVertices = self.numVertices + 1
+        return newVertex
+        
+    def getVertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+            
+    def addEdge(self, f, t, cost=0):
+        if f not in self.vertList:
+            new_vertex = Vertex(f)
+        if t not in self.vertList:
+            new_vertex = Vertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t].getId(),cost)
+    
+    def printGraph(self):
+        print "Vertex list:"
+        print self.vertList.keys()
+        
+        for id in self.vertList.keys():
+            vertex = self.vertList[id]
+            print "Vertex: " + str(self.vertList[id].getId())
+            print self.vertList[id].connectedTo
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
